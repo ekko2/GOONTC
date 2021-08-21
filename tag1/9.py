f1bb3f2d6eb1afb3a0c1caa71906e021cb12ca35
ekko2 <18701012836@163.com>
2021-08-22 01:15:24 +0800

    3nd

---
+++ tag1/9.py
@@ -0,0 +1,46 @@
+# 给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+# 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+#
+#
+# 示例 1：
+#
+# 输入：digits = "23"
+# 输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+# 示例 2：
+#
+# 输入：digits = ""
+# 输出：[]
+# 示例 3：
+#
+# 输入：digits = "2"
+# 输出：["a","b","c"]
+#
+# 提示：
+#
+# 0 <= digits.length <= 4
+# digits[i] 是范围 ['2', '9'] 的一个数字。
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        def dfs(cur, dic, ans):
+            if len(cur) == len(digits):
+                ans.append(cur)
+                return
+            n = len(cur)
+            print(n)
+            # print(digits[n])
+            # print(int(digits[n])-2)
+            for i in range(len(dic[int(digits[n]) - 2])):
+                cur += dic[int(digits[n]) - 2][i]
+                dfs(cur, dic, ans)
+                cur = cur[:len(cur) - 1]
+                print(cur)
+
+        if not digits:
+            return
+        dic = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+        ans = []
+        dfs('', dic, ans)
+        return ans
